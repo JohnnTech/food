a29dedd377a18a6654fd2697604fd64f670c6ad6
JohnnTech <132626922+JohnnTech@users.noreply.github.com>
2024-01-24 13:28:59 -0300

    criação do produto-vitrine

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,14 +1,24 @@
 import Navbar from "../../Components/navbar/navbar.jsx";
-
+import ProdutoVitrine from "../../Components/produto-vitrine/produto-vitrine.jsx";
 
 function Home() {
     return <>
        <Navbar />
-       Nosso Cardápio <br />
-       Clique aqui para colocar is produtos na  sacola de compras.
-       Se preferir, você pode pedir pelo WhatsApp: (61) 994525-54285 <br />
-       Lista de produtos...
+       <div className="container">
+            <div className="titulo text-center">
+                <h1>Nosso Cardápio</h1>
+                <p className="subtitulo">Clique aqui para colocar is produtos na  sacola de compras.
+                Se preferir, você pode pedir pelo WhatsApp: (61) 994525-54285 <br />
+                </p>
+            </div>
 
+       </div>
+       <div className="text-center">
+            <ProdutoVitrine/>
+            <ProdutoVitrine/>
+            <ProdutoVitrine/>
+            <ProdutoVitrine/>
+        </div>
     </>
 }
 
