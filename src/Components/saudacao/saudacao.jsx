8ff9d427be4782018de26ce7f93d4f0db06ef7d7
JohnnTech <132626922+JohnnTech@users.noreply.github.com>
2024-01-23 23:25:40 -0300

    criando o component saudacao

---
+++ src/Components/saudacao/saudacao.jsx
@@ -0,0 +1,7 @@
+
+
+function Saudacao(){
+    return <h2>Olá John</h2>
+  }
+
+export default Saudacao
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import Saudacao from './Components/saudacao/saudacao';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-  <h1>Hello World</h1>
-  <h2>Hello World</h2>
+  <Saudacao/>
   </>
 
 );
